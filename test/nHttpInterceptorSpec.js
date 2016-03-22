d24a4e38280c7a41354016a88908644ba4122bc9
Alexander Hafstad <alexander@hafstad.dk>
2016-03-22 14:31:44 +0100

    test(nHttpInterceptor): Added tests for nHttpInterceptor

---
+++ test/nHttpInterceptorSpec.js
@@ -0,0 +1,59 @@
+describe('nHttpInterceptor', function() {
+
+	var nHttpInterceptorProvider;
+	var nHttpInterceptor;
+
+	beforeEach(function() {
+		module('nCore.nHttpInterceptor.provider');
+	});
+
+	// What should the feature do?
+	it('should return defaults', function() {
+
+		inject(['nHttpInterceptor', function(_nHttpInterceptor) {
+			nHttpInterceptor = _nHttpInterceptor; // to use the instance in other parts
+		}]);
+
+		// What is the actual output?
+		var actual = nHttpInterceptor;
+
+		// What is the expected output?
+		var expected = {
+			errorMessages: {
+				error304: 'Not modified',
+				error401: 'Unauthorized',
+				error403: 'Forbidden',
+				error404: 'Not found',
+				error412: 'Precondition failed',
+				error440: 'No accept header',
+				error441: 'No token was provided',
+				error442: 'Invalid token',
+				error443: 'Token has expired',
+				error445: 'Invalid 3rd party token',
+				error500: 'Interal server error'
+			}
+		};
+
+		expect(actual).toEqual(expected);
+	});
+
+	// What should the feature do?
+	it('should configure custom error message', function() {
+
+		// load the provider with module to be able to call its configuration methods
+		module(['nHttpInterceptorProvider', function(_nHttpInterceptorProvider) {
+			nHttpInterceptorProvider = _nHttpInterceptorProvider; // to use the provider in other parts
+			nHttpInterceptorProvider.configure({error0: 'Oops! Something went wrong'});
+		}]);
+
+		inject(['nHttpInterceptor', function(_nHttpInterceptor) {
+			nHttpInterceptor = _nHttpInterceptor; // to use the instance in other parts
+		}]);
+
+		// What is the actual output?
+		var actual = nHttpInterceptor;
+
+		// What is the expected output?
+		expect(actual.errorMessages.error0).toBeDefined();
+	});
+});
